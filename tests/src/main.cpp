daf06ff628d59c9340c2788dcbb62378bac095ac
Eric Yanush <ericyanush@gmail.com>
2018-02-24 14:12:49 -0600

    Setup for unit tests

---
+++ tests/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Eric Yanush on 2018-02-24.
+//
+
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
